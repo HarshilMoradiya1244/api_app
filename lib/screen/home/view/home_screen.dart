5916572c6c5d4adbd80c52a47895914300c5ddaf
vivek <Katariyavivek228@gmail.com>
2024-01-05 14:49:23 +0530

    new

--- lib/screen/home/view/home_screen.dart
+++ lib/screen/home/view/home_screen.dart
@@ -24,48 +24,48 @@ class _HomeScreenState extends State<HomeScreen> {
     providerw = context.watch<HomeProvider>();
     return SafeArea(
       child: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            title: const Text("Country News"),
-          ),
-          body: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  buildActionChip(name: "USA", countryCode: 'us'),
-                  buildActionChip(name: "INDIA", countryCode: 'in'),
-                  buildActionChip(name: "CANADA", countryCode: 'ca'),
-                  buildActionChip(name: "AUS", countryCode: 'au'),
-                ],
-              ),
-              providerw!.statusModel == null
-                  ? const Center(child: CircularProgressIndicator())
-                  : Expanded(
-                      child: ListView.builder(
-                        itemCount: providerw!.statusModel!.articalList!.length,
-                        itemBuilder: (context, index) {
-                          return Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: ListTile(
-                                leading: Image.network(providerw!.statusModel!
-                                        .articalList![index].urlToImage ??
-                                    "https://yt3.googleusercontent.com/y_esGAQOhX4rTpWvrALErAJlFbm_2TIVrvcVfcZny7TuA8dJZgOQcC6KRfd_J5hljFe-foYXj9U=s900-c-k-c0x00ffffff-no-rj"),
-                                title: Text(
-                                  "${providerw!.statusModel!.articalList![index].title}",
-                                  style: const TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 15),
-                                ),
-                                onTap: () {
-                                  Navigator.pushNamed(context, 'tesla');
-                                },
-                              ));
-                        },
-                      ),
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Country News"),
+        ),
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                buildActionChip(name: "USA", countryCode: 'us'),
+                buildActionChip(name: "INDIA", countryCode: 'in'),
+                buildActionChip(name: "CANADA", countryCode: 'ca'),
+                buildActionChip(name: "AUS", countryCode: 'au'),
+              ],
+            ),
+            providerw!.statusModel == null
+                ? const Center(child: CircularProgressIndicator())
+                : Expanded(
+                    child: ListView.builder(
+                      itemCount: providerw!.statusModel!.articalList!.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: ListTile(
+                              leading: Image.network(providerw!.statusModel!
+                                      .articalList![index].urlToImage ??
+                                  "https://yt3.googleusercontent.com/y_esGAQOhX4rTpWvrALErAJlFbm_2TIVrvcVfcZny7TuA8dJZgOQcC6KRfd_J5hljFe-foYXj9U=s900-c-k-c0x00ffffff-no-rj"),
+                              title: Text(
+                                "${providerw!.statusModel!.articalList![index].title}",
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 15),
+                              ),
+                              onTap: () {
+                                Navigator.pushNamed(context, 'tesla');
+                              },
+                            ));
+                      },
                     ),
-            ],
-          )),
+                  ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -73,7 +73,9 @@ class _HomeScreenState extends State<HomeScreen> {
           {required String name, required String countryCode}) =>
       ActionChip(
         label: Text(name),
-        backgroundColor: providerr!.selectedCountry == countryCode?Colors.blue:Colors.white,
+        backgroundColor: providerr!.selectedCountry == countryCode
+            ? Colors.blue
+            : Colors.white,
         onPressed: () {
           providerr!.Country(countryCode);
           providerr!.getData();
